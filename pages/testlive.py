cb29498222a64d222418ccf9919cd1f27352fd5b
IamTheKaz <kazwolff85@gmail.com>
2025-07-25 20:37:49 -0700

    Update testlive.py

--- pages/testlive.py
+++ pages/testlive.py
@@ -2,4 +2,9 @@ from streamlit_webrtc import webrtc_streamer
 import streamlit as st
 
 st.title("WebRTC Live Stream Test")
-webrtc_streamer(key="live")
+webrtc_streamer(
+    key="example",
+    rtc_configuration={
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    }
+)
