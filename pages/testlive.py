29e4aa99d56def2569ae43dfb8e932ba7cd4d9db
IamTheKaz <kazwolff85@gmail.com>
2025-07-25 20:49:12 -0700

    test live

--- pages/testlive.py
+++ pages/testlive.py
@@ -1,10 +1,16 @@
-from streamlit_webrtc import webrtc_streamer
 import streamlit as st
+from streamlit_webrtc import webrtc_streamer
 
-st.title("WebRTC Live Stream Test")
+st.title("🚀 WebRTC Live Stream Test")
+
 webrtc_streamer(
-    key="example",
+    key="live_stream",
     rtc_configuration={
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    }
+    },
+    media_stream_constraints={
+        "video": True,
+        "audio": False
+    },
+    async_processing=True,
 )
